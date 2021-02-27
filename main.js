8978bb42067b2d42b90bdcb84e62431d77af2c9d
carolyne <pcmaunda@gmail.com>
2021-02-27 22:48:11 +0300

    updated code

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+
+
+
 document.getElementById("btn").addEventListener("click",  function(e)
 
 {
 e.preventDefault()
- let year = document.getElementById("year").value;
- let month = document.getElementById("month").value;
+ 
  let date = document.getElementById("date").value; 
- let newResult = `${year},${month}, ${date}`;
+ let newResult = date;
 
  let d = new Date(newResult);
  let day = d.getDay()
@@ -15,58 +17,59 @@ e.preventDefault()
     var genderMale = document.querySelector("#male").value;
     var genderFemale = document.querySelector("#female").value;
   
+  if(genderMale == "male" || genderFemale =="female"){
+    alert('Your Akan name is' +" "+ malesNames[day] + "\r\n" + "You were born on" + " " + malesDays[day])
+  }
+  else  {
+  
+   alert(femalesNames[day] + "\n " + "You were born on" + " " + femalesDays[day])
+  }
+  })
+
   
 
-    
-const malesDays = [
-    "Sunday",
-    "Monday", 
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-
-const malesNames =[
-"Kwasi", 
-"kwadwo",
-"kwabena",
-"kwaku",
-"yaw",
-"Kofi",
-"Kwame"
-];
-const femalesNames = [
-"Sunday" ,
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",
-];
-
-const femalesDays = [
-  "Akosua",
-  "Adwoa",
-  "Abenaa",
-  "AKua",
-  "Yaa",
-  "Afua",
-  "Ama"
-];
-if(genderMale == "male" || genderFemale =="female"){
-  alert('Your Akan name is' +" "+ malesNames[day] + "\ " + "You were born on" + " " + malesDays[day])
-}
-else  {
-
- alert(femalesNames[day] + "\ " + "You were born on" + " " + femalesDays[day])
-}
-})
+ const malesDays = [
+      "Sunday",
+      "Monday", 
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+  ];
+  
+  const malesNames =[
+  "Kwasi", 
+  "kwadwo",
+  "kwabena",
+  "kwaku",
+  "yaw",
+  "Kofi",
+  "Kwame"
+  ];
+  const femalesNames = [
+  "Sunday" ,
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  ];
+  
+  const femalesDays = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "AKua",
+    "Yaa",
+    "Afua",
+    "Ama"
+  ];
 
 
-function myFunction() {
-  var x = document.getElementById("myform").min;
-  document.getElementById("date").innerHTML = x;
-}
+  
+  function myFunction() {
+    var x = document.getElementById("myform").min;
+    document.getElementById("date").innerHTML = x;
+  }
